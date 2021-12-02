e982e4e45ffd2758784c760277e63d3ac6d920d9
Leul Yiheyis <leulykpro@gmail.com>
2021-12-02 11:02:10 +0300

    reuse the height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,28 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_height - computes height of a binary tree
+ *
+ * @tree: pointer to the root node
+ *
+ * Return: computed height, 0 if tree is NULL
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+	return (left_height > right_height ? left_height : right_height);
+}
+
 /**
  * binary_tree_balance - Function caculates the balance factor of a tree
  *
@@ -19,9 +41,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		l_height = binary_tree_balance(tree->left) + 1;
+		l_height = binary_tree_height(tree->left);
 	if (tree->right)
-		r_height = binary_tree_balance(tree->right) + 1;
+		r_height = binary_tree_height(tree->right);
+	if (!l_height && tree->left)
+		l_height = 1;
+	if (!r_height && tree->right)
+		r_height = 1;
 
 	return (l_height - r_height);
 }
