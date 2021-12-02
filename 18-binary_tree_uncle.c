5d06b9ca848de07ceea0e91a7fee995353c317d3
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-12-02 17:15:30 +0300

    Add Function to find uncle/aunt of node
    For task 18 of Project: - binary_trees

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_node_uncle - Function finds the uncle/aunt of a node
+ *
+ * @node : Pointer to the node to find the uncle
+ *
+ * Return: Pointer to uncle/aunt node, NULL if @node is NULL or there is no
+ * uncle.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	int i = 2;
+	binary_tree_t *current_node = node;
+
+	while (i--)
+	{
+		if (node->parent)
+			node = node->parent;
+		else
+			return (NULL);
+	}
+
+	if (node->left && current_node->parent != node->left)
+		return (node->left);
+	if (node->right)
+		return (node->right);
+
+	return (NULL);
+}
